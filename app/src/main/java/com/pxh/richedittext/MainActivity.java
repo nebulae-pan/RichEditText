dff642ab59dec249136f9f053dacf17617cbf740
nebulae-pan <nebula0929@gmail.com>
2016-04-28 23:13:17 +0800

    4/28

--- app/src/main/java/com/pxh/richedittext/MainActivity.java
+++ app/src/main/java/com/pxh/richedittext/MainActivity.java
@@ -1,12 +1,9 @@
 package com.pxh.richedittext;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
