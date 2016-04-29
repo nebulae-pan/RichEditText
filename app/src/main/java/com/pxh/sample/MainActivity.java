0e284a437c8774422f36b42c9078a4672fb296cc
nebulae-pan <nebula0929@gmail.com>
2016-04-29 21:22:01 +0800

    4/29

--- app/src/main/java/com/pxh/sample/MainActivity.java
+++ app/src/main/java/com/pxh/sample/MainActivity.java
@@ -1,10 +1,12 @@
-package com.pxh.richedittext;
+package com.pxh.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.pxh.richedittext.RichEditText;
 
 public class MainActivity extends AppCompatActivity
 {
---
+++ rich_edittext/src/main/java/com/pxh/richedittext/RichEditorImageGetter.java
@@ -0,0 +1,16 @@
+package com.pxh.richedittext;
+
+import android.graphics.drawable.Drawable;
+import android.text.Html;
+
+/**
+ * Created by pxh on 2016/4/29.
+ */
+public class RichEditorImageGetter implements Html.ImageGetter
+{
+    @Override
+    public Drawable getDrawable(String source)
+    {
+        return null;
+    }
+}
