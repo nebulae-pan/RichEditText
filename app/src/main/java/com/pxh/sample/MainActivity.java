5a561fdfce3c73d71fe9f8fb6d4996b210992cca
nebulae-pan <nebula0929@gmail.com>
2016-05-07 18:24:25 +0800

    5/7

--- app/src/main/java/com/pxh/sample/MainActivity.java
+++ app/src/main/java/com/pxh/sample/MainActivity.java
@@ -3,7 +3,6 @@ package com.pxh.sample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
